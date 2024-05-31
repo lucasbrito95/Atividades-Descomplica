7bfd349dfc58db1cf1402278d07eadb4360636ac
Lucas Brito95 <158345053+lucasbrito95@users.noreply.github.com>
2024-05-31 15:11:22 -0300

    Add files via upload

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,26 @@
+import React, {useState, useEffect} from 'react';
+import { FormControl, InputLabel, Input, FormHelperText } from '@mui/material';
+
+
+const Login = () => {
+  return (
+
+  );
+}
+const [login, setlogin] = useState('');
+<Grid item xs={12}>
+<FormControl fullWidth>
+  <Input id="login_nome" aria-describedby="login_nome_helper_text" value={login} 
+  onChange={e => { setTituloTarefa(e.target.value) }} />
+  <FormHelperText id="login_nome_helper_text">Login.</FormHelperText>
+</FormControl>
+</Grid>
+
+<Grid item xs={12}>  
+<FormControl fullWidth>
+  <Input id="tarefa_descricao" aria-describedby="tarefa_descricao_helper_text" value={descricaoTarefa} onChange={e => { setDescricaoTarefa(e.target.value) }} />
+  <FormHelperText id="tarefa_descricao_helper_text">Descrição da Tarefa.</FormHelperText>
+</FormControl>
+</Grid>
+
+export default Login;
